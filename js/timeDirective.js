5ddbffd9a736f8562f4bcae71f40cf154203a2f2
marcandy <marcandy.nj@gmail.com>
2016-12-20 12:12:13 -0700

    progress

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -1,7 +1,11 @@
 angular.module('timeApp')
-  .directive(function () {
+  .directive('showTime', function () {
 
     return {
-      
+      restrict: 'EA',
+      template: "<div> The current time: {{time | date:'short'}} </div>", //use date filter to format the time
+      link: function (scope, element, attrs) {
+        scope.time = new Date();
+      }
     }
   })
